ae830094b876fac16e9e334f153f7309f6e9f50f
Blessing Onwunali <chikaonwunali2019@gmail.com>
2023-08-05 11:28:36 +0100

    a

--- GUI/JFrameQuestion1.java
+++ GUI/JFrameQuestion1.java
@@ -1,6 +1,6 @@
 package GUI;
 
-import java.awt.GridLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,32 +12,48 @@ public class JFrameQuestion1 extends JFrame{
     JButton btn = new JButton("AC Milan");
     JButton btn2 = new JButton("Real Madrid");
     JLabel label = new JLabel("Result: ");
-    JLabel label2 = new JLabel("");
+    JLabel label2 = new JLabel("Winner:");
     JLabel label3 = new JLabel("");
 
     int count = 0;
+    int count2 = 0;
 
     public JFrameQuestion1(){
         super("Match");
         add(btn);
         add(btn2);
-        setLayout(new GridLayout());
-        setSize(500, 400);
+        add(label);
+        add(label2);
+        add(label3);
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        setSize(300, 300);
         setVisible(true);
-        btn.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent ae){
-                count++;
-                label.setText(count + " X 0");
-            }
-        });
+        
+        InnerClass listener = new InnerClass();
+        btn.addActionListener(listener);
+        btn2.addActionListener(listener);
+    }
 
-        btn2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae){
+    class InnerClass implements ActionListener{
+        public void actionPerformed(ActionEvent ae){
+            if(ae.getSource() == btn){
                 count++;
-                label.setText("0 X " + count);
+                label.setText("Result: " + count + " X " + count2);
+            } else {
+                count2++;
+                label.setText("Result: " + count + " X " + count2);
             }
-        });
 
+
+
+            if (count > count2) {
+                label2.setText("Winner: AC Milan ");
+            } else if (count < count2) {
+                label2.setText("Winner: Real Madrid");
+            } else {
+                label2.setText("Winner: DRAW");
+            }
+        }
     }
 
     public static void main(String[] args) {
