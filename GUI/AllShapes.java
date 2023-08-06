7bc96895ec5b66202699eefa92a2f9a87eff0052
Blessing Onwunali <chikaonwunali2019@gmail.com>
2023-08-06 22:46:18 +0100

    b

---
+++ GUI/AllShapes.java
@@ -0,0 +1,21 @@
+package GUI;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class AllShapes extends JFrame {
+    AllShapes(){
+        super("All Shapes");
+    }
+    public static void main(String[] args) {
+        new AllShapes();
+    }
+    public void paint(Graphics g){
+        super.paint(g);
+        g.drawLine(100, 20, 200, 100);
+        g.drawRect(5, 150, 150, 100);
+        g.drawRoundRect(165, 150, 150, 100, 15, 15);
+        setTitle(getWidth() + " " + getHeight());
+    }
+}
---
+++ GUI/ThreeLinkedFrames.java
@@ -0,0 +1,41 @@
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class ThreeLinkedFrames {
+    public static void main(String[] args) {
+        // Create three JFrame objects
+        JFrame frame1 = createFrame("Frame 1", 400, 300);
+        JFrame frame2 = createFrame("Frame 2", 400, 300);
+        JFrame frame3 = createFrame("Frame 3", 400, 300);
+
+        // Calculate the positions for the second and third frames
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+
+        int x1 = frame1.getX();
+        int y1 = frame1.getY();
+        int x2 = x1 + frame1.getWidth();
+        int y2 = y1 + frame1.getHeight();
+
+        int x3 = x2 < screenWidth ? x2 : x1;
+        int y3 = y2 < screenHeight ? y2 : y1;
+
+        // Set the positions of the second and third frames
+        frame2.setLocation(x2, y2);
+        frame3.setLocation(x3, y3);
+
+        // Make the frames visible
+        frame1.setVisible(true);
+        frame2.setVisible(true);
+        frame3.setVisible(true);
+    }
+
+    private static JFrame createFrame(String title, int width, int height) {
+        JFrame frame = new JFrame(title);
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return frame;
+    }
+}
