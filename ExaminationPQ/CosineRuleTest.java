5717716aa5169207e157efd80ee8b72d577c2c02
Blessing Onwunali <chikaonwunali2019@gmail.com>
2023-09-02 02:15:41 +0100

    a

--- ExaminationPQ/CosineRuleTest.java
+++ ExaminationPQ/CosineRuleTest.java
@@ -6,7 +6,8 @@ public class CosineRuleTest{
     public static void main(String[] args){
         CosineRule cosRule = new CosineRule();
         cosRule.setDefaultCloseOperation(
-            JFrame.EXIT_ON_CLOSE);
+            JFrame.EXIT_ON_CLOSE
+        );
         cosRule.setLocationRelativeTo(null);
         cosRule.setVisible(true);
     }//end of main()
