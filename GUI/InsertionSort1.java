50e8e1707352db808744ba7e40e7e7ffa080bbc3
Blessing Onwunali <chikaonwunali2019@gmail.com>
2023-09-03 01:41:20 +0100

    b

---
+++ GUI/InsertionSort1.java
@@ -0,0 +1,24 @@
+public class InsertionSort1 {
+    public static void main(String[] args) {
+        int array[] = {2, 5, 3, 7, 4, 9, 8, 6, 1};
+
+        insertionSort(array);
+
+        for(int 1 : array){
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void insertionSort(int[] array){
+        for(int i =0; i < array.length; i++){
+            int temp = array[i];
+            int j = i - 1;
+
+            while(j > = 0 && array[j] > temp){
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+    }
+}
