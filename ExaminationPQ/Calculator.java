b6d4ef1f1ad7c55d4eba0d8605620ae773aefe12
Blessing Onwunali <chikaonwunali2019@gmail.com>
2023-09-05 01:13:43 +0100

    a

--- ExaminationPQ/Calculator.java
+++ ExaminationPQ/Calculator.java
@@ -26,5 +26,32 @@ public class Calculator extends JFrame{
         setLayout(new FlowLayout());
         setSize(400, 250);
         setVisible(true);
+
+        box = new JTextField(10);
+        btn1 = new JButton("1");
+        btn2 = new JButton("2");
+        btn3 = new JButton("3");
+        btnAdd = new JButton("+");
+        btn4 = new JButton("4");
+        btn5 = new JButton("5");
+        btn6 = new JButton("6");
+        btnSubtract = new JButton("-");
+        btn7 = new JButton("7");
+        btn8 = new JButton("8");
+        btn9 = new JButton("9");
+        btnMultiply = new JButton("*");
+        btnDivision = new JButton("/");
+        btnZero = new JButton("0");
+        btnEqual = new JButton("=");
+
+        add(btn1);
+        add(btn2);
+        add(btn3);
+        add(btnEqual)
+        add(btn4);
+        add(btn5);
+        add(btn6);
+        add(btnSubtract);
+        add(btn7)
     }
 }
